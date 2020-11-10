cd8372543af87b4d3dbd2be470432f10d22de013
Renzo Salinas <renzos.f15@gmail.com>
2020-11-10 15:01:34 -0500

    Agregando MVC

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Programacion2020Teoria.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
